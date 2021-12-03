ebf41d07e495fa395893743fd68569ea1a8b0f85
Liza Machukha <94099022+istepmother@users.noreply.github.com>
2021-12-03 12:27:02 +0200

    Update 2-improved.cpp

--- sum/2-improved.cpp
+++ sum/2-improved.cpp
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int addition(int x, int y){
+int addition(int x, int y) {
     return x + y;
 }
 
-int main()
-{
+int main() {
     int sum = addition(2, 3);
     printf("%d\n", sum);
 }
